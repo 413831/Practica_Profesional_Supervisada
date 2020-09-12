1c751c99a464d84552f439815dded3132e12e5f6
413831 <alepleins3pp@gmail.com>
2020-09-12 10:45:51 -0300

    Esqueletos proyectos

---
+++ 1_Edificio/src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'folder/Inbox',
+    pathMatch: 'full'
+  },
+  {
+    path: 'folder/:id',
+    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'bonitas',
+    loadChildren: () => import('./pages/bonitas/bonitas.module').then( m => m.BonitasPageModule)
+  },
+  {
+    path: 'feas',
+    loadChildren: () => import('./pages/feas/feas.module').then( m => m.FeasPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
