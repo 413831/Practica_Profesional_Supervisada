dbcc7ceac12aa8d56d31ef5294a93ac71c19f221
413831 <alepleins3pp@gmail.com>
2020-09-14 16:01:21 -0300

    Se agrega Cordova/Capacitor

--- 2_Idiomas/src/app/app-routing.module.ts
+++ 2_Idiomas/src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
   },
   {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
   }
 ];
 @NgModule({
---
+++ 2_Idiomas/src/app/services/data.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Usuario } from '../interfaces/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private usuarios: Usuario[] = [];
+  private static idUsuario = 0;
+
+  constructor(private firebaseAuth: AngularFireAuth) 
+  {
+  }
+
+  login(usuario: Usuario)
+  {
+    // return this.test(usuario);
+    console.log(usuario);
+    return new Promise<any>((resolve, reject) => {
+      this.firebaseAuth.signInWithEmailAndPassword(usuario.email, usuario.pass)
+                        .then(response => resolve(response),error => reject(error));
+    });
+  }
+
+  registrar(usuario: Usuario)
+  {
+    return new Promise<any>((resolve,reject) => {
+      this.firebaseAuth.createUserWithEmailAndPassword(usuario.email, usuario.pass)
+                        .then(response => resolve(response), error => reject(error));
+    });
+
+  }
+
+  gerUserDetail()
+  {
+    return this.firebaseAuth.currentUser;
+  }
+
+}
