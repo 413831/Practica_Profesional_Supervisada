95fe2f216b115413dac9e40797142f4188af138f
413831 <alepleins3pp@gmail.com>
2020-09-16 19:31:00 -0300

    Update iconos

--- 4_EscanerQR/android/app/src/main/java/io/ionic/escanerqr/MainActivity.java
+++ 4_EscanerQR/android/app/src/main/java/io/ionic/escanerqr/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package io.ionic.escanerqr;
 
 import android.os.Bundle;
 
--- 5_Chat/android/app/src/main/java/io/ionic/chat/MainActivity.java
+++ 5_Chat/android/app/src/main/java/io/ionic/chat/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package io.ionic.chat;
 
 import android.os.Bundle;
 
